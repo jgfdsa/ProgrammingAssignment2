54b1966318ab7a36e954def835d339a5212d18a3
Gregorio <jgfdsa@gmail.com>
2014-08-24 15:37:35 +0200

    1st commit to master
    Added initial description

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As a part of the course R programming, assignment 2:
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix; based on the example "Caching the Mean of a Vector and
+## creates a special "vector", which is really a list containing a functionto
+##      1. set the value of the vector
+##      2. get the value of the vector
+##      3. set the value of the mean
+##      4. get the value of the mean
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
